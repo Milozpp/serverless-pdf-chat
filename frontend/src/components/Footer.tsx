91052fcc4922ac37993205516d4696f162d39dee
Milo Zappulla <milo.zappulla2@external.stellantis.com>
2024-10-17 14:54:34 +0200

    changed color

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,18 +8,9 @@ const Footer: React.FC = () => {
         <div className=" flex flex-row justify-between py-3 text-sm">
           <div className="inline-flex items-center">
             <CloudIcon className="w-5 h-5 mr-1.5" />
-            Powered by Amazon Web Services
+            Powered by Storm Reply
           </div>
-          <div className="inline-flex items-center hover:underline underline-offset-2">
-            <img
-              src={GitHub}
-              alt="React Logo"
-              width={20}
-              className="mr-1.5 py-2 mx-2"
-            />
-            <a href="https://github.com/aws-samples/serverless-pdf-chat">
-              Source code on GitHub
-            </a>
+          <div className="inline-flex items-center hover:underline underline-offset-2">    
           </div>
         </div>
       </footer>
